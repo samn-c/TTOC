dfb5fbf778bf4d189a0dd8c13edf7a72bfb79108
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-09-13 19:51:37 -0400

    Added time offset section on time zone converter page

--- js/time.js
+++ js/time.js
@@ -6,5 +6,11 @@ function localTime() {
 function UTC() {
     document.getElementById("utc").innerHTML = a.toUTCString();
 }
+
+function offset() {
+    let offset = a.getTimezoneOffset() / 60;
+    document.getElementById("offset").innerHTML = offset + " hours";
+}
 setInterval(localTime, 1000);
 setInterval(UTC, 1000);
+setInterval(offset, 1000);
